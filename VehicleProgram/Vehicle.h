9ed752fbad52a3192fec8cc968808ae7f309fab7
MahalaCovey <MahalaCovey@users.noreply.github.com>
2024-04-04 13:17:15 -0400

    Initial Vehicle.h file

---
+++ VehicleProgram/Vehicle.h
@@ -0,0 +1,29 @@
+// Mahala Covey
+// CIS 1202 800
+// April 4, 2024
+
+// Specification file for the Vehicle class
+#ifndef VEHICLE_H
+#define VEHICLE_H
+#include <string>
+using namespace std;
+
+class Vehicle
+{
+private:
+	// Member variables
+	string manufacturer; 
+	int year;
+
+public:
+	Vehicle(); // Default constructor
+	Vehicle(string, int); // Constructor
+	void displayInfo();
+	// Setters
+	void setManufacturer(string);
+	void setYear(int);
+	// Getters
+	string getManufacturer();
+	int getYear();
+};
+#endif
